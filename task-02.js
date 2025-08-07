276efebcd2260a62d114a2e35f5f592961e7f0fa
mdrifatulislam <mdrifatulislam1080@gmail.com>
2025-08-07 17:03:59 +0600

    added task-02.js

---
+++ task-02.js
@@ -0,0 +1,9 @@
+const numbers = [12, 98, 5, 41, 23, 78, 46];
+const evenNumbers = [];
+for (let i = 0; i < numbers.length; i++) {
+  const number = numbers[i];
+  if (number % 2 === 0) {
+    evenNumbers.push(number);
+  }
+}
+console.log("even number:", evenNumbers);
